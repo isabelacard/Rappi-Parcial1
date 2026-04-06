026ca06dbc3ff29271cf941531a4abc7efa5f212
Isabela Cardona <1111481156@u.icesi.edu.co>
2026-04-05 22:42:34 -0500

    fix/feat: Add docs folder and fix env errors

--- server/db.js
+++ server/db.js
@@ -1,11 +1,12 @@
 import { Pool } from "pg";
+import "dotenv/config";
 
 export const pool = new Pool({
-    user: "postgres.xxbmibngzxgpmxinnzoi",
-    host: "aws-1-us-east-1.pooler.supabase.com",
-    database: "postgres",
-    password: "Danielitomipuki",
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: Number(process.env.DB_PORT),
     keepAlive: true,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 2000,
